97d45af39768b058832bdb02dbcf7a3e5fc55ab8
Dhia Abbassi <dhiaabbassi90@gmail.com>
2014-09-14 17:16:28 +0100

    Added preview modal

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,41 @@
 var app = angular.module("app", ['ui.bootstrap']);
 
-app.controller("mainCtrl", function($scope, $http, $filter) {
+app.controller("mainCtrl", function($scope, $http, $filter, $modal, $log) {
+
+	$scope.open = function (url) {
+		$scope.url = url;
+	    var modalInstance = $modal.open({
+	      templateUrl: url,
+	      controller: modalCtrl,
+	      size: 'lg',
+	      resolve: {
+	        items: function () {
+	          return $scope.url;
+	        }
+	      }
+	    });
+
+	    modalInstance.result.then(function (selectedItem) {
+	      $scope.selected = selectedItem;
+	    }, function () {
+	      $log.info('Modal dismissed at: ' + new Date());
+	    });
+    };
+
+    var modalCtrl = function ($scope, $modalInstance, items) {
+
+			$scope.items = items;
+			$scope.selected = {
+				item: $scope.items[0]
+			};
+
+			$scope.download = function () {
+				$modalInstance.close($scope.selected.item);
+			};
+
+			$scope.cancel = function () {
+				$modalInstance.dismiss('cancel');
+			};
+	};
 
 })
